1fe4a939195d91d70364ee2477d49c25442dac56
jhclaura <linkinmonkey@gmail.com>
2021-05-30 14:47:19 -0400

    Static website

---
+++ wp-content/themes/equilibrium/styles/custom.php
@@ -0,0 +1,4 @@
+/*	Custom Styles
+	....................................................................................................................................... */
+
+	html body { background: #ffffff url('/wp-content/themes/equilibrium/images/layout/right_strip.png') repeat left top; }
